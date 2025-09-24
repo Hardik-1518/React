56746b9c81a88889b1fab36a38213ff973dca7cd
Hardik Srivastava <hardiksatwik2004@gmail.com>
2025-09-24 11:02:15 +0530

    TODO FINAL

---
+++ projects/todoapp2.0/src/components/WelcomeMessage.jsx
@@ -0,0 +1,7 @@
+import styles from "./WelcomeMessage.module.css";
+
+const WelcomeMessage = () => {
+  return <p className={styles.welcome}>Enjoy Your Day 😊</p>;
+};
+
+export default WelcomeMessage;
