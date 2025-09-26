dc96418d412fedc5e4b0d8bc613fe1df6a164434
Hardik Srivastava <hardiksatwik2004@gmail.com>
2025-09-26 21:09:30 +0530

    useContext

--- projects/todoapp2.0/src/components/AddTodo.jsx
+++ projects/todoapp2.0/src/components/AddTodo.jsx
@@ -1,38 +1,50 @@
-import { useRef } from "react";
-import { BiMessageAdd } from "react-icons/bi";
+import { useContext } from "react";
+import { useState } from "react";
+import { TodoItemsContext } from "../store/todo-items-store";
 
-function AddTodo({ onNewItem }) {
-  const todoNameElement = useRef();
-  const dueDateElement = useRef();
+function AddTodo() {
+  const { addNewItem } = useContext(TodoItemsContext);
+  const [todoName, setTodoName] = useState();
+  const [dueDate, setDueDate] = useState();
 
-  const handleAddButtonClicked = (event) => {
-    event.preventDefault();
-    const todoName = todoNameElement.current.value;
-    const dueDate = dueDateElement.current.value;
-    todoNameElement.current.value = "";
-    dueDateElement.current.value = "";
-    onNewItem(todoName, dueDate);
+  const handleNameChange = (event) => {
+    setTodoName(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setDueDate(event.target.value);
+  };
+
+  const handleAddButtonClicked = () => {
+    addNewItem(todoName, dueDate);
+    setDueDate("");
+    setTodoName("");
   };
 
   return (
     <div className="container text-center">
-      <form className="row kg-row" onSubmit={handleAddButtonClicked}>
+      <div className="row kg-row">
         <div className="col-6">
           <input
             type="text"
-            ref={todoNameElement}
             placeholder="Enter Todo Here"
+            value={todoName}
+            onChange={handleNameChange}
           />
         </div>
         <div className="col-4">
-          <input type="date" ref={dueDateElement} />
+          <input type="date" value={dueDate} onChange={handleDateChange} />
         </div>
         <div className="col-2">
-          <button type="submit" className="btn btn-success kg-button">
-            <BiMessageAdd />
+          <button
+            type="button"
+            className="btn btn-success kg-button"
+            onClick={handleAddButtonClicked}
+          >
+            Add
           </button>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
--- projects/todoapp2.0/src/components/TodoItem.jsx
+++ projects/todoapp2.0/src/components/TodoItem.jsx
@@ -1,6 +1,9 @@
-import { AiFillDelete } from "react-icons/ai";
+import { useContext } from "react";
+import { TodoItemsContext } from "../store/todo-items-store";
 
-function TodoItem({ todoName, todoDate, onDeleteClick }) {
+function TodoItem({ todoName, todoDate }) {
+  const { deleteItem } = useContext(TodoItemsContext);
+
   return (
     <div className="container">
       <div className="row kg-row">
@@ -10,9 +13,9 @@ function TodoItem({ todoName, todoDate, onDeleteClick }) {
           <button
             type="button"
             className="btn btn-danger kg-button"
-            onClick={() => onDeleteClick(todoName)}
+            onClick={() => deleteItem(todoName)}
           >
-            <AiFillDelete />
+            Delete
           </button>
         </div>
       </div>
